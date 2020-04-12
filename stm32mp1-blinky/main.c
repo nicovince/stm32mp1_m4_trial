8613f2b69411fc828e9417558324cafade57d107
Nicolas VINCENT <nicolas.vincent@vossloh.com>
2020-04-12 10:04:38 +0200

    stm32mp1 bliny: increase nb nop between led toggle
    tested with ST code, 2000000 nop seems to do the trick. Unfortunately
    this still does not work, probably linking is not done correctly.

--- stm32mp1-blinky/main.c
+++ stm32mp1-blinky/main.c
@@ -8,11 +8,13 @@
 #define PORT_LED7 GPIOH
 #define PIN_LED7  7
 
-#define LITTLE_BIT 100000
+#define LITTLE_BIT 2000000
 
 int main(void) {
     rcc_periph_clock_enable(RCC_LED7);
-    gpio_mode_setup(PORT_LED7, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, PIN_LED7);
+    gpio_mode_setup(PORT_LED7, GPIO_MODE_OUTPUT, GPIO_PUPD_PULLUP, PIN_LED7);
+    gpio_set_output_options(PORT_LED7, GPIO_OTYPE_PP,
+                            GPIO_OSPEED_25MHZ,  PIN_LED7);
     gpio_set(PORT_LED7, PIN_LED7);
     while(1) {
         /* wait a little bit */
