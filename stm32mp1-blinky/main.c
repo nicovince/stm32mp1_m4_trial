c18623670a3eb4d4e560f6f67e81bff6e2fedebd
Nicolas VINCENT <nicolas.vincent@vossloh.com>
2020-04-13 15:46:55 +0200

    stm32mp1: Fix blinky
    use gpio mask instead of gpio number
    increase delay to slow down blinking

--- stm32mp1-blinky/main.c
+++ stm32mp1-blinky/main.c
@@ -7,21 +7,22 @@
 #define RCC_LED7  RCC_GPIOH
 #define PORT_LED7 GPIOH
 #define PIN_LED7  7
+#define GPIO_LED7 (1 << PIN_LED7)
 
-#define LITTLE_BIT 2000000
+#define LITTLE_BIT 10000000
 
 int main(void) {
     rcc_periph_clock_enable(RCC_LED7);
-    gpio_mode_setup(PORT_LED7, GPIO_MODE_OUTPUT, GPIO_PUPD_PULLUP, PIN_LED7);
+    gpio_mode_setup(PORT_LED7, GPIO_MODE_OUTPUT, GPIO_PUPD_PULLUP, GPIO_LED7);
     gpio_set_output_options(PORT_LED7, GPIO_OTYPE_PP,
-                            GPIO_OSPEED_25MHZ,  PIN_LED7);
-    gpio_set(PORT_LED7, PIN_LED7);
+                            GPIO_OSPEED_25MHZ,  GPIO_LED7);
+    gpio_set(PORT_LED7, GPIO_LED7);
     while(1) {
         /* wait a little bit */
         for (int i = 0; i < LITTLE_BIT; i++) {
             __asm__("nop");
         }
-        gpio_toggle(PORT_LED7, PIN_LED7);
+        gpio_toggle(PORT_LED7, GPIO_LED7);
     }
     return 0;
 }
