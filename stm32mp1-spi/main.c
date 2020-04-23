54c535dcdbcd85f9f776b6d203948701bb3fad29
Nicolas VINCENT <nicolas.vincent@vossloh.com>
2020-04-23 15:39:17 +0200

    spi: Clear flag whenever possible

--- stm32mp1-spi/main.c
+++ stm32mp1-spi/main.c
@@ -31,6 +31,13 @@ int main(void) {
     usart_send_printf(USART3, "SPI2S_SR: %x\r\n", SPI2S_SR(SPI4));
 
     while(1) {
+        spi_sr = SPI2S_SR(SPI4);
+        if (spi_sr & SPI2S_SR_EOT) {
+            spi_clear_flags(SPI4, SPI2S_IFCR_EOTC);
+        }
+        if (spi_sr & SPI2S_SR_TXTF) {
+            spi_clear_flags(SPI4, SPI2S_IFCR_TXTFC);
+        }
         if (last_ts + 5000 < mtime()) {
             usart_send_printf(USART3, "----- [%d] -----\r\n", mtime());
             led_toggle();
@@ -44,9 +51,6 @@ int main(void) {
             usart_send_printf(USART3, "CFG2:%x\r\n", SPI_CFG2(SPI4));
             spi_sr = SPI2S_SR(SPI4);
             usart_send_printf(USART3, "SPI2S_SR: %x\r\n", spi_sr);
-            if (spi_sr & SPI2S_SR_EOT) {
-                spi_clear_flags(SPI4, SPI2S_IFCR_EOTC | SPI2S_IFCR_TXTFC);
-            }
             usart_send_printf(USART3, "SPI2S_SR: %x\r\n", SPI2S_SR(SPI4));
             last_ts = mtime();
 
