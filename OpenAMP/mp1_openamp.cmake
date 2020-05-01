9f1088835f236aaac26c00d74c4548a0abd377f5
Nicolas VINCENT <nicolas.vincent@vossloh.com>
2020-05-01 16:29:34 +0200

    Remove absolute path

--- OpenAMP/mp1_openamp.cmake
+++ OpenAMP/mp1_openamp.cmake
@@ -2,11 +2,11 @@ set (CROSS_PREFIX           "arm-none-eabi-" CACHE STRING "")
 set (CMAKE_SYSTEM_PROCESSOR "arm" CACHE STRING "")
 set (MACHINE "metal" CACHE STRING "")
 
-set (CMAKE_C_FLAGS "-mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -Og -fmessage-length=0 -ffunction-sections -I/home/nicolas/work/siema/vendors/ST/stm32mp1_m4_trial/OpenAMP/libmetal/build/output/usr/local/include -c" CACHE STRING "")
-set (CMAKE_FIND_ROOT_PATH /home/nicolas/work/siema/vendors/ST/stm32mp1_m4_trial/OpenAMP/libmetal/build/output/usr/local/lib )
+set (LIBMETAL_INCLUDE_DIR ../../libmetal/build/output/usr/local/lib )
+set (LIBMETAL_HDR_DIR ../../libmetal/build/output/usr/local/include )
+set (CMAKE_C_FLAGS "-mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -Og -fmessage-length=0 -ffunction-sections -I${LIBMETAL_HDR_DIR} -c" CACHE STRING "")
+set (CMAKE_FIND_ROOT_PATH ${LIBMETAL_INCLUDE_DIR} )
 
-
-set (LIBMETAL_INCLUDE_DIR /home/nicolas/work/siema/vendors/ST/stm32mp1_m4_trial/OpenAMP/libmetal/build/output/usr/local/lib )
 include (cross_generic_gcc)
 
 # vim: expandtab:ts=2:sw=2:smartindent
